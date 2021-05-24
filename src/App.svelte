7aa6ecaec140b5090d2ac877113054b6b29e071f
Mikael Lönn <mikael@sendify.se>
2021-05-24 10:29:53 +0200

    Inital image gallery

---
+++ src/App.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  import lightGallery from 'lightGallery';
+  import lgThumbnail from 'lightgallery/plugins/thumbnail';
+  import lgZoom from 'lightgallery/plugins/zoom';
+  import { onMount } from 'svelte';
+  import Image from './Image.svelte';
+
+  onMount(() => {
+    lightGallery(document.getElementById('gallery-container'), {
+      speed: 500,
+      plugins: [lgZoom, lgThumbnail],
+    });
+  });
+</script>
+
+<main>
+  <h1>Albin Stratus</h1>
+  <div id="gallery-container">
+    <Image image="main1" />
+    <Image image="main2" />
+    <Image image="kitchen1" />
+    <Image image="kitchen2" />
+    <Image image="kitchen3" />
+    <Image image="back" />
+    <Image image="front1" />
+    <Image image="front2" />
+    <Image image="bathroom" />
+    <Image image="batteries" />
+    <Image image="battery_charge" />
+    <Image image="battery_voltage" />
+  </div>
+</main>
+
+<style>
+  @import url('https://cdn.jsdelivr.net/npm/lightgallery@2.0.0-beta.4/css/lightgallery.css');
+  @import url('https://cdn.jsdelivr.net/npm/lightgallery@2.0.0-beta.4/css/lg-zoom.css');
+  @import url('https://cdn.jsdelivr.net/npm/lightgallery@2.0.0-beta.4/css/lg-thumbnail.css');
+  :global(body) {
+    background-color: rgb(40, 44, 52);
+    color: white;
+  }
+  main {
+    display: grid;
+    align-items: center;
+    text-align: center;
+    justify-content: center;
+  }
+
+  #gallery-container {
+    max-width: 1280px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+</style>
---
+++ src/Image.svelte
@@ -0,0 +1,22 @@
+<!-- svelte-ignore a11y-missing-attribute -->
+<script lang="ts">
+  export let image: string;
+  const base = 'https://res.cloudinary.com/dzf3v5esf/image/upload';
+  const cloudinaryId = 'v1621842386/Serenity';
+  const url = `${base}/${cloudinaryId}/${image}.jpg`;
+  const thumbnail = `${base}/h_250,c_scale/${cloudinaryId}/${image}.jpg`;
+</script>
+
+<a data-src={url}>
+  <img class="img-responsive" alt={image} src={thumbnail} />
+</a>
+
+<style>
+  a {
+    margin-right: 4px;
+    margin-bottom: 4px;
+  }
+  img {
+    display: block;
+  }
+</style>
